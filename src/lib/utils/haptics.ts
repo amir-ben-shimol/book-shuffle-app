e4932d6f9ab744c991196db1c81aa3832ef57b13
amir-ben-shimol <amir.benshi@gmail.com>
2024-07-23 20:36:41 +0300

    refactor: Add delay before triggering haptic feedback on long press

--- src/lib/utils/haptics.ts
+++ src/lib/utils/haptics.ts
@@ -1,6 +1,6 @@
 import * as Haptics from 'expo-haptics';
 
-const hapticTimings = [800, 600, 500, 400, 300, 300, 300, 200, 200, 200, 200];
+const hapticTimings = [800, 600, 500, 400, 300, 200, 200, 200, 150, 150, 100, 100, 100, 100, 100, 50, 50];
 
 export const onLongHaptics = (onComplete: () => void) => {
 	let index = 0;
